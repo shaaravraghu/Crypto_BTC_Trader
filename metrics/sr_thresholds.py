137992bdedf381ab1d7912f5ddbdf67f4639dfb0
Shaarav Raghu <shaaravraghu@gmail.com>
2026-05-14 17:17:49 +0530

    Clean up blank lines in sr_thresholds.py
    Removed excessive blank lines from the sr_thresholds.py file.

--- metrics/sr_thresholds.py
+++ metrics/sr_thresholds.py
@@ -52,23 +52,3 @@ def process(current_price: float, sr_levels: dict, context: str, points_to_award
             "levels_analyzed": list(sr_levels.keys())
         }
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
